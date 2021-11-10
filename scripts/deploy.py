afeb7bc02f16aae402f049a318b650bc1f386bd2
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-11-10 16:33:05 -0500

    update deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,13 +1,13 @@
-from brownie import Election, SolidityStorage, VyperStorage, accounts, network
-
+from brownie import Election, accounts, network
 
 def main():
     # requires brownie account to have been created
     if network.show_active()=='development':
         # add these accounts to metamask by importing private key
         owner = accounts[0]
-        Election.deploy({'from':accounts[0]})
+        Election.deploy({'from': owner})
 
     elif network.show_active() == 'kovan':
         # add these accounts to metamask by importing private key
-        owner = accounts.load("main")
+        owner = accounts.load('main')
+        Election.deploy({'from': owner})
