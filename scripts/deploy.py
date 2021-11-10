0c2a9bfe5bb5a556c196e00133ad7ab5122d5225
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-11-10 16:21:19 -0500

    remove unnecessary code

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,12 +6,8 @@ def main():
     if network.show_active()=='development':
         # add these accounts to metamask by importing private key
         owner = accounts[0]
-        SolidityStorage.deploy({'from':accounts[0]})
-        VyperStorage.deploy({'from':accounts[0]})
         Election.deploy({'from':accounts[0]})
 
     elif network.show_active() == 'kovan':
         # add these accounts to metamask by importing private key
         owner = accounts.load("main")
-        SolidityStorage.deploy({'from':owner})
-        VyperStorage.deploy({'from':owner})
--- tests/conftest.py
+++ tests/conftest.py
@@ -10,22 +10,6 @@ def setup(fn_isolation):
     pass
 
 
-@pytest.fixture(scope="module")
-def vyper_storage(accounts, VyperStorage):
-    """
-    Yield a `Contract` object for the VyperStorage contract.
-    """
-    yield accounts[0].deploy(VyperStorage)
-
-
-@pytest.fixture(scope="module")
-def solidity_storage(accounts, SolidityStorage):
-    """
-    Yield a `Contract` object for the SolidityStorage contract.
-    """
-    yield accounts[0].deploy(SolidityStorage)
-
-
 @pytest.fixture(scope='module')
 def election(accounts, Election):
     """
